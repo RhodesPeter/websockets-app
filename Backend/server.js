9cdb8543b165c71277008df3899abffc65546386
Peter Rhodes <rhodespeter@hotmail.co.uk>
2020-03-25 17:15:39 +0000

    Change port for Heroku

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,7 @@ var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
