555a067f28afbd0eb7ab34c889dc42f8873b6563
Peter Rhodes <rhodespeter@hotmail.co.uk>
2020-03-26 11:03:32 +0000

    Add chat app styling

--- Public/scripts/index.js
+++ Public/scripts/index.js
@@ -3,9 +3,12 @@ var socket = io();
 document.querySelector('form').addEventListener('submit', function (e) {
   e.preventDefault(); // prevents page reloading
   var textInput = document.querySelector('.chat__text-input');
-  socket.emit('chat message', textInput.value);
-  textInput.value = '';
-  return false;
+
+  if (textInput.value.length > 0) {
+    socket.emit('chat message', textInput.value);
+    textInput.value = '';
+    return false;
+  }
 });
 
 socket.on('chat message', function (msg) {
