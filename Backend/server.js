beab7054570647179103891fd2862ee10a8e0b12
Peter Rhodes <rhodespeter@hotmail.co.uk>
2020-03-26 16:13:39 +0000

    Break up server and sockets files

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var sockets = require('./sockets');
 
 const port = process.env.PORT || 3000;
 
@@ -11,36 +12,8 @@ app.get('/', function (req, res) {
 
 app.use(express.static('Public'))
 
-io.on('connection', function (socket) {
-  console.log('a user connected');
-  io.emit('chat message', 'A new user has joined the chat');
-
-  socket.on('chat message', function (msg) {
-    console.log('message: ' + msg);
-
-    // This will emit the event to all connected sockets
-    io.emit('chat message', msg);
-  });
-
-  emitUpdateUsersEvent();
-
-  socket.on('disconnect', function () {
-    console.log('a user disconnected');
-    io.emit('chat message', 'A user has left the chat');
-
-    emitUpdateUsersEvent();
-  });
-});
-
-const emitUpdateUsersEvent = () => {
-  // This '/' path is the path of the page with the websockets connection on
-  io.of('/').clients((error, clients) => {
-    if (error) throw error;
-    console.log(clients);
-    io.emit('connected users', clients);
-  });
-};
-
 http.listen(port, function () {
   console.log(`listening on port ${port}`);
 });
+
+sockets(io);
---
+++ Backend/sockets.js
@@ -0,0 +1,32 @@
+const sockets = (io) => {
+  io.on('connection', function (socket) {
+    console.log('a user connected');
+    io.emit('chat message', 'A new user has joined the chat');
+
+    socket.on('chat message', function (msg) {
+      console.log('message: ' + msg);
+
+      // This will emit the event to all connected sockets
+      io.emit('chat message', msg);
+    });
+
+    emitUpdateUsersEvent(io);
+
+    socket.on('disconnect', function () {
+      console.log('a user disconnected');
+      io.emit('chat message', 'A user has left the chat');
+
+      emitUpdateUsersEvent(io);
+    });
+  });
+}
+
+const emitUpdateUsersEvent = (io) => {
+  // This '/' path is the path of the page with the websockets connection on
+  io.of('/').clients((error, clients) => {
+    if (error) throw error;
+    io.emit('connected users', clients);
+  });
+};
+
+module.exports = sockets;
